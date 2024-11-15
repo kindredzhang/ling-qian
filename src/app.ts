819bc3c1880ff5b911351b3b82a67f625d1bbf59
kindredzhang <kindred.zhang.life@gmail.com>
2024-11-15 22:11:15 +0800

    commit

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import path from 'path';
 import ganZhiRouter from './routes/ganZhiRouter';
 
 const app = express();
+app.use(express.static(path.join(__dirname, '../public')));
+
 const port = process.env.PORT || 8035;
 
 app.use(express.json());
